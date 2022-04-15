a26c6dc64d00a6a1dac1bd7965043245bbcb2bd1
Ronnie <ronniebercero@gmail.com>
2022-04-16 02:21:33 +0800

    Add conditional statement to playRound

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection.toLowerCase() && computerSelection.toLowerCase() == "rock") {
+        return "Its a Draw!!";
+    } else if (playerSelection.toLowerCase() && computerSelection.toLowerCase() == "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() && computerSelection.toLowerCase() == "scissors") {
+        return "You Win! Rock beats Scissors";
+    }
 }
 
 const playerSelection = "rock";
