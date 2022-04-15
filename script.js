b27e6a19392f4623f1cd09560e9defa5b704fcdf
Ronnie <ronniebercero@gmail.com>
2022-04-16 03:22:12 +0800

    Add console.log

--- script.js
+++ script.js
@@ -16,3 +16,4 @@ function playRound(playerSelection, computerSelection) {
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
