d7584ffee4fbef0a12ec606839a5a2ea2d524a9c
Ronnie <ronniebercero@gmail.com>
2022-04-20 23:04:36 +0800

    Move playRound inside play function

--- script.js
+++ script.js
@@ -10,33 +10,33 @@ function playerSelect() {
 }
 
 function play() {
-    
-}
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "Its a Draw!";
-    } else {
-        //Validate player selection Rock.
-        if (playerSelection == "rock" && computerSelection == "paper") {
-            return "You Lose! Paper beats Rock";
-        } else if (playerSelection == "rock" && computerSelection == "scissors") {
-            return "You Win! Rock beats Scissors";
-        }
-        //Validate player selection Paper.
-        if (playerSelection == "paper" && computerSelection == "scissors") {
-            return "You Lose! Scissors beats Paper";
-        } else if (playerSelection == "paper" && computerSelection == "rock") {
-            return "You Win! Paper beats Rock";
-        }
-        //Validate player selection Scissors.
-        if (playerSelection == "scissors" && computerSelection == "rock") {
-            return "You Lose! Rock beats Scissors";
-        } else if (playerSelection == "scissors" && computerSelection == "paper") {
-            return "You Win! Scissors beats Paper";
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return "Its a Draw!";
+        } else {
+            //Validate player selection Rock.
+            if (playerSelection == "rock" && computerSelection == "paper") {
+                return "You Lose! Paper beats Rock";
+            } else if (playerSelection == "rock" && computerSelection == "scissors") {
+                return "You Win! Rock beats Scissors";
+            }
+            //Validate player selection Paper.
+            if (playerSelection == "paper" && computerSelection == "scissors") {
+                return "You Lose! Scissors beats Paper";
+            } else if (playerSelection == "paper" && computerSelection == "rock") {
+                return "You Win! Paper beats Rock";
+            }
+            //Validate player selection Scissors.
+            if (playerSelection == "scissors" && computerSelection == "rock") {
+                return "You Lose! Rock beats Scissors";
+            } else if (playerSelection == "scissors" && computerSelection == "paper") {
+                return "You Win! Scissors beats Paper";
+            }
         }
     }
 }
+
 
 const playerSelection = playerSelect();
 const computerSelection = computerSelect();
